ddc34bf6d214998181667fb1b15f68fa40cc26ce
Chad Malla <chad.malla@sbisecsol.com>
2020-06-11 18:52:19 -0700

    add simple counter example and code snippet for react state demo

--- lifecycles.js
+++ lifecycles.js
@@ -1,25 +1,27 @@
-'use strict';
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
+class Counter extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { liked: false };
+      super(props);
+      this.state = {
+          counter: 0 
+      }
+      this.increment = () => this.setState({counter: this.state.counter + 1});
+      this.decrement = () => this.setState({counter: this.state.counter - 1});
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+      console.log('Render');
+      return (
+          <div>
+              <button onClick={this.increment}>Increment</button>
+              <button onClick={this.decrement}>Decrement</button>
+              <div className="counter">
+                  Counter: {this.state.counter}
+              </div>
+          </div>
+      )
   }
 }
 
 const domContainer = document.querySelector('#state_demo');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<Counter />, domContainer);
